6cc8067bb0175ccba832b6fae5243e3d0bc97983
j <136126244+shopper12@users.noreply.github.com>
2026-09-05 15:34:24 +0900

    Refresh Amsterdam itinerary cache

--- travel-live.js
+++ travel-live.js
@@ -1,4 +1,4 @@
-import { DEFAULT_ITINERARY, ITINERARIES } from "./itinerary-data.js?v=LIVE_TRAVEL_V17";
+import { DEFAULT_ITINERARY, ITINERARIES } from "./itinerary-data.js?v=AMSTERDAM_LIVE_20260905_V1";
 
 const LIVE_URL = "./trip-live.json";
 const FARES_URL = "./flight-prices.json";
